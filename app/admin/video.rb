f277d7a3a37badfbccb470a4f2345ee41a9b0ae8
Raghu Betina <raghu@firstdraft.com>
2017-08-24 22:03:47 +0000

    Whitelist columns in Video for ActiveAdmin

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Video do
 
+ permit_params :caption, :videolink, :starttime, :user_id, :platform_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
