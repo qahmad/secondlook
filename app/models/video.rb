b8212124d8dc1389a40d957359d717d629f227f4
Raghu Betina <raghu@firstdraft.com>
2017-08-24 22:02:59 +0000

    Validate presence of videos.starttime

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,8 @@ class Video < ApplicationRecord
 
   # Validations
 
+  validates :starttime, :presence => true
+
   validates :starttime, :numericality => { :greater_than => 1 }
 
   validates :videolink, :uniqueness => { :scope => [:starttime, :user_id], :message => "You have already linked this video at this start time! Please add a different video or enter a different start time." }
