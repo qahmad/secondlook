ca383fc1d6d14d16f5f4503d1484de2b1e8fca39
Raghu Betina <raghu@firstdraft.com>
2017-08-24 22:02:59 +0000

    Validate numericality of videos.starttime

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,8 @@ class Video < ApplicationRecord
 
   # Validations
 
+  validates :starttime, :numericality => { :greater_than => 1 }
+
   validates :videolink, :uniqueness => { :scope => [:starttime, :user_id], :message => "You have already linked this video at this start time! Please add a different video or enter a different start time." }
 
   validates :videolink, :presence => { :message => "Please enter a video link" }
