b3fd20e63053dad415de8512d6d3846ccb4d3f44
Raghu Betina <raghu@firstdraft.com>
2017-08-24 22:04:00 +0000

    Whitelist columns in Platform for ActiveAdmin

--- app/admin/platform.rb
+++ app/admin/platform.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Platform do
 
+ permit_params :name, :embed_link
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
